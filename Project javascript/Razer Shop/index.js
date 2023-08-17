291bf313c9b89465e5d47b9cc7d9fa98bcbf8094
phuchit321 <142424497+phuchit321@users.noreply.github.com>
2023-08-17 20:29:09 +0700

    Add files via upload

---
+++ Project javascript/Razer Shop/index.js
@@ -0,0 +1,277 @@
+var product = [{
+    id: 1,
+    img: 'https://assets3.razerzone.com/fV7qvwe6--8mgqkwp58GZQCl3k4=/1500x1000/https%3A%2F%2Fhybrismediaprod.blob.core.windows.net%2Fsys-master-phoenix-images-container%2Fh0f%2Fhaf%2F9143289970718%2Fhuntsman-v2-analog-1500x1000-20210204-05.jpg',
+    name: 'Razer Hunsman V2 Analog',
+    price: '8669',
+    description: 'keybord ipsum dolor sit, amet consectetur adipisicing elit. Voluptates ea labore perferendis, similique corporis laudantium. Fuga at vero voluptas sequi',
+    type: 'keybord'
+}, {
+    id: 2,
+    img: 'https://assets3.razerzone.com/ZvGdt1_GYwzaYNsnh2kQlfhzxFo=/1500x1000/https%3A%2F%2Fhybrismediaprod.blob.core.windows.net%2Fsys-master-phoenix-images-container%2Fha7%2Fh93%2F9248880263198%2F211021-kraken-v3-pro-1500x1000-6.jpg',
+    name: 'Razer headphone',
+    price: '5000',
+    description: 'headphone ipsum dolor sit, amet consectetur adipisicing elit. Voluptates ea labore perferendis, similique corporis laudantium. Fuga at vero voluptas sequi',
+    type: 'headphone'
+}, {
+    id: 3,
+    img: 'https://assets3.razerzone.com/5cq_TBflGc65trHeURQi-QO6xY8=/1500x1000/https%3A%2F%2Fhybrismediaprod.blob.core.windows.net%2Fsys-master-phoenix-images-container%2Fh5b%2Fhc9%2F9592950980638%2F230628-cobra-pro-mouse-dock-pro-1500x1000-1.jpg',
+    name: 'Razer Cobra Pro + Mouse Dock Pro Bundle',
+    price: '5898',
+    description: 'mouse ipsum dolor sit, amet consectetur adipisicing elit. Voluptates ea labore perferendis, similique corporis laudantium. Fuga at vero voluptas sequi',
+    type: 'mouse'
+}, {
+    id: 4,
+    img: 'https://assets3.razerzone.com/CBPjFkeMie1z_s3eiouSXbqNZCA=/1500x1000/https%3A%2F%2Fhybrismediaprod.blob.core.windows.net%2Fsys-master-phoenix-images-container%2Fheb%2Fh05%2F9434919862302%2F220823-basilisk-v3-pro-1500x1000-1.jpg',
+    name: 'Razer Basilisk V3 Pro - Black',
+    price: '5548',
+    description: 'mouse ipsum dolor sit, amet consectetur adipisicing elit. Voluptates ea labore perferendis, similique corporis laudantium. Fuga at vero voluptas sequi',
+    type: 'mouse'
+}, {
+    id: 5,
+    img: 'https://assets3.razerzone.com/_gD3IdhInq1H0W8Ry7r0N6C1N7M=/1500x1000/https%3A%2F%2Fhybrismediaprod.blob.core.windows.net%2Fsys-master-phoenix-images-container%2Fh22%2Fhcd%2F9450422534174%2F221110-naga-v2-pro-2-1500x1000-6.jpg',
+    name: 'Razer Naga V2 Pro',
+    price: '6241',
+    description: 'mouse ipsum dolor sit, amet consectetur adipisicing elit. Voluptates ea labore perferendis, similique corporis laudantium. Fuga at vero voluptas sequi',
+    type: 'mouse'
+}, {
+    id: 6,
+    img: 'https://assets3.razerzone.com/8LF488v7ks55Nm4GZ4EWelZzgcs=/1500x1000/https%3A%2F%2Fhybrismediaprod.blob.core.windows.net%2Fsys-master-phoenix-images-container%2Fh8d%2Fh1e%2F9451886673950%2F221110-naga-v2-hyperspeed-1500x1000-4.jpg',
+    name: 'Razer Naga V2 HyperSpeed',
+    price: '3467',
+    description: 'mouse ipsum dolor sit, amet consectetur adipisicing elit. Voluptates ea labore perferendis, similique corporis laudantium. Fuga at vero voluptas sequi',
+    type: 'mouse'
+}, {
+    id: 7,
+    img: 'https://assets3.razerzone.com/baaFmkHq6CvTjd5yu8qelYFEcYM=/1500x1000/https%3A%2F%2Fhybrismediaprod.blob.core.windows.net%2Fsys-master-phoenix-images-container%2Fh72%2Fh48%2F9481236086814%2F230221-da-v3-1500X1000-2.jpg',
+    name: 'Razer DeathAdder V3',
+    price: '2426',
+    description: 'mouse ipsum dolor sit, amet consectetur adipisicing elit. Voluptates ea labore perferendis, similique corporis laudantium. Fuga at vero voluptas sequi',
+    type: 'mouse'
+}, {
+    id: 8,
+    img: 'https://assets3.razerzone.com/RpUeXnt4tNXUKhKXFgAECtspNSE=/1500x1000/https%3A%2F%2Fhybrismediaprod.blob.core.windows.net%2Fsys-master-phoenix-images-container%2Fhbc%2Fhe8%2F9081238388766%2Fhuntsman-mini-black-1.jpg',
+    name: 'Razer Huntsman Mini - Clicky Optical Switch',
+    price: '4161',
+    description: 'mouse ipsum dolor sit, amet consectetur adipisicing elit. Voluptates ea labore perferendis, similique corporis laudantium. Fuga at vero voluptas sequi',
+    type: 'keybord'
+}, {
+    id: 9,
+    img: 'https://assets3.razerzone.com/f_nir8TTNXFkNMacLUkZUEyUqnM=/1500x1000/https%3A%2F%2Fhybrismediaprod.blob.core.windows.net%2Fsys-master-phoenix-images-container%2Fh5c%2Fhc2%2F9239674748958%2F16-sep-huntsman-v2-tkl-1500x1000-3.jpg',
+    name: 'Razer Huntsman V2 Tenkeyless - Clicky Optical Switch',
+    price: '5201',
+    description: 'mouse ipsum dolor sit, amet consectetur adipisicing elit. Voluptates ea labore perferendis, similique corporis laudantium. Fuga at vero voluptas sequi',
+    type: 'keybord'
+}, {
+    id: 10,
+    img: 'https://assets3.razerzone.com/iA-LbTJ3ikhhZ2GTNcIotXaKo8A=/1500x1000/https%3A%2F%2Fhybrismediaprod.blob.core.windows.net%2Fsys-master-phoenix-images-container%2Fh36%2Fh7a%2F9261073006622%2Frazer-blackwidow-v3-1500x1000-0-V3.jpg',
+    name: 'Razer BlackWidow V3 - Yellow Switch',
+    price: '4854',
+    description: 'mouse ipsum dolor sit, amet consectetur adipisicing elit. Voluptates ea labore perferendis, similique corporis laudantium. Fuga at vero voluptas sequi',
+    type: 'keybord'
+}, {
+    id: 11,
+    img: 'https://assets3.razerzone.com/2f9BLDJNhqZVuLtJPLtySz9i_Pw=/1500x1000/https%3A%2F%2Fhybrismediaprod.blob.core.windows.net%2Fsys-master-phoenix-images-container%2Fhb1%2Fh65%2F9180986310686%2Frazer-blackwidow-v3-mini-hyperspeed-1500x1000-01.jpg',
+    name: 'Razer BlackWidow V3 Mini HyperSpeed',
+    price: '4854',
+    description: 'mouse ipsum dolor sit, amet consectetur adipisicing elit. Voluptates ea labore perferendis, similique corporis laudantium. Fuga at vero voluptas sequi',
+    type: 'keybord'
+}, {
+    id: 12,
+    img: 'https://assets3.razerzone.com/NoOLfb_PcCjFndA5ZPT3g0ji7_g=/1500x1000/https%3A%2F%2Fhybrismediaprod.blob.core.windows.net%2Fsys-master-phoenix-images-container%2Fh0a%2Fh5b%2F9248879411230%2F211021-kraken-v3-1500x1000-1.jpg',
+    name: 'Razer Kraken V3',
+    price: '3467',
+    description: 'mouse ipsum dolor sit, amet consectetur adipisicing elit. Voluptates ea labore perferendis, similique corporis laudantium. Fuga at vero voluptas sequi',
+    type: 'headphone'
+}, {
+    id: 13,
+    img: 'https://assets3.razerzone.com/pCmPWu7e6oy3iSjE6qbfXPD4IoE=/1500x1000/https%3A%2F%2Fhybrismediaprod.blob.core.windows.net%2Fsys-master-phoenix-images-container%2Fh38%2Fhe6%2F9081217581086%2FPhoenix-Gallery-Hero-BlackSharkV2-1500x1000-2.jpg',
+    name: 'Razer BlackShark V2 - Black',
+    price: '3467',
+    description: 'mouse ipsum dolor sit, amet consectetur adipisicing elit. Voluptates ea labore perferendis, similique corporis laudantium. Fuga at vero voluptas sequi',
+    type: 'headphone'
+}, {
+    id: 14,
+    img: 'https://assets3.razerzone.com/ds8gETxX3VAHyqZ8XQenfJL_vw0=/1500x1000/https%3A%2F%2Fhybrismediaprod.blob.core.windows.net%2Fsys-master-phoenix-images-container%2Fh70%2Fh47%2F9242259030046%2F210928-blackshark-v2-x-green-1500x1000-3.jpg',
+    name: 'Razer BlackShark V2 X - Green',
+    price: '2080',
+    description: 'mouse ipsum dolor sit, amet consectetur adipisicing elit. Voluptates ea labore perferendis, similique corporis laudantium. Fuga at vero voluptas sequi',
+    type: 'headphone'
+}];
+
+$(document).ready(() => {
+    var html = '';
+    for (let i = 0; i < product.length; i++) {
+        html += `<div onclick="openProductDetail(${i})" class="product-items ${product[i].type}">
+                    <img class="product-img" src="${product[i].img}" alt="">
+                    <p style="font-size: 1.2vw;">${product[i].name}</p>
+                    <p stlye="font-size: 1vw;">${numberWithCommas(product[i].price)} THB</p>
+                </div>`;
+    }
+    $("#productlist").html(html);
+
+})
+
+function numberWithCommas(x) {
+    x = x.toString();
+    var pattern = /(-?\d+)(\d{3})/;
+    while (pattern.test(x))
+        x = x.replace(pattern, "$1,$2");
+    return x;
+}
+
+function searchsomething(elem) {
+    // console.log('#'+elem.id)
+    var value = $('#' + elem.id).val()
+    console.log(value)
+
+    var html = '';
+    for (let i = 0; i < product.length; i++) {
+        if (product[i].name.includes(value)) {
+            html += `<div onclick="openProductDetail(${i})" class="product-items ${product[i].type}">
+                    <img class="product-img" src="${product[i].img}" alt="">
+                    <p style="font-size: 1.2vw;">${product[i].name}</p>
+                    <p stlye="font-size: 1vw;">${numberWithCommas(product[i].price)} THB</p>
+                </div>`;
+        }
+    }
+    if (html == '') {
+        $("#productlist").html(`<p>Not found product</p>`);
+    } else {
+        $("#productlist").html(html);
+    }
+
+}
+
+function searchproduct(param) {
+    console.log(param)
+    $(".product-items").css('display', 'none')
+    if (param == 'all') {
+        $(".product-items").css('display', 'block')
+    }
+    else {
+        $("." + param).css('display', 'block')
+    }
+}
+
+var productindex = 0;
+function openProductDetail(index) {
+    productindex = index;
+    console.log(productindex)
+    $("#modalDesc").css('display', 'flex')
+    $("#mdd-img").attr('src', product[index].img);
+    $("#mdd-name").text(product[index].name)
+    $("#mdd-price").text(numberWithCommas(product[index].price) + ' THB')
+    $("#mdd-desc").text(product[index].description)
+}
+
+function closeModal() {
+    $(".modal").css('display', 'none')
+}
+
+var cart = [];
+function addtocart() {
+    var pass = true;
+
+    for (let i = 0; i < cart.length; i++) {
+        if (productindex == cart[i].index) {
+            console.log('found same product')
+            cart[i].count++;
+            pass = false;
+        }
+    }
+
+    if (pass) {
+        var obj = {
+            index: productindex,
+            id: product[productindex].id,
+            name: product[productindex].name,
+            price: product[productindex].price,
+            img: product[productindex].img,
+            count: 1
+        };
+        // console.log(obj)
+        cart.push(obj)
+    }
+    console.log(cart)
+
+    Swal.fire({
+        icon: 'success',
+        title: 'Add ' + product[productindex].name + ' to cart !'
+    })
+    $("#cartcount").css('display', 'flex').text(cart.length)
+}
+
+function openCart() {
+    $('#modalCart').css('display', 'flex')
+    rendercart();
+}
+
+function rendercart() {
+    if (cart.length > 0) {
+        var html = '';
+        for (let i = 0; i < cart.length; i++) {
+            html += `<div class="cartlist-item">
+                        <div class="cartlist-left">
+                            <img src="${cart[i].img}">
+                            <div class="cartlist-detail">
+                                <p style="font-size: 1.5vw;">${cart[i].name}</p>
+                                <p style="font-size: 1.2vw;">${numberWithCommas(cart[i].price * cart[i].count)}</p>
+                            </div>
+                        </div>
+                        <div class="cartlist-right">
+                            <p onclick="deinitems('-', ${i})" class="btnc">-</p>
+                            <p id="countitems${i}" style="margin: 0 20px;">${cart[i].count}</p>
+                            <p onclick="deinitems('+', ${i})" class="btnc">+</p>
+                        </div>
+                    </div>`;
+        }
+        $("#mycart").html(html)
+    }
+    else {
+        $("#mycart").html(`<p>Not found product list</p>`)
+    }
+}
+
+function deinitems(action, index) {
+    if (action == '-') {
+        if (cart[index].count > 0) {
+            cart[index].count--;
+            $("#countitems" + index).text(cart[index].count)
+
+            if (cart[index].count <= 0) {
+                Swal.fire({
+                    icon: 'warning',
+                    title: 'Are you sure to delete?',
+                    showConfirmButton: true,
+                    showCancelButton: true,
+                    confirmButtonText: 'Delete',
+                    cancelButtonText: 'Cancel'
+                }).then((res) => {
+                    if (res.isConfirmed) {
+                        cart.splice(index, 1)
+                        console.log(cart)
+                        rendercart();
+                        $("#cartcount").css('display', 'flex').text(cart.length)
+
+                        if (cart.length <= 0) {
+                            $("#cartcount").css('display', 'none')
+                        }
+                    }
+                    else {
+                        cart[index].count++;
+                        $("#countitems" + index).text(cart[index].count)
+                        rendercart();
+                    }
+                })
+            }
+            rendercart();
+        }
+
+    }
+    else if (action == '+') {
+        cart[index].count++;
+        $("#countitems" + index).text(cart[index].count)
+        rendercart();
+    }
+}
+
